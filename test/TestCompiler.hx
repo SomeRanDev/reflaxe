7be2a5a63333edee720055e0f2ea66120d8b48c6
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-03-15 19:06:21 -0400

    Added helpful comments to test/TestCompiler

--- test/TestCompiler.hx
+++ test/TestCompiler.hx
@@ -1,22 +1,39 @@
 package;
 
+// Your compiler can be made to only exist at compile-time, but it's up to you!
 #if macro
 
+// Import relevant Haxe macro types.
 import haxe.macro.Expr;
 import haxe.macro.Type;
 
+// Required Reflaxe types.
 import reflaxe.ReflectCompiler;
 import reflaxe.BaseCompiler;
 
+// Reflaxe has a ton of "helper" classes with static extensions.
+// A few are used here.
 import reflaxe.helpers.OperatorHelper;
 using reflaxe.helpers.SyntaxHelper;
 using reflaxe.helpers.ModuleTypeHelper;
 using reflaxe.helpers.NameMetaHelper;
 
+// This is a tool built into Reflaxe for modifying functions
+// before typing using @:build macros. 
 import reflaxe.input.ClassModifier;
 
 class TestCompiler extends BaseCompiler {
+	
+	// This is the initialization macro used to make the compiler work!
+	// It can be a static function in any class, but we place it in
+	// the "TestCompiler" class for convenience. 
+	//
+	// Use `--macro TestCompiler.Start()` in your hxml to run!
 	public static function Start() {
+
+		// Pass an instance to `reflaxe.ReflectCompiler` using AddCompiler to activate it.
+		// There's a ton of options that can be used.
+		// Visit the reflaxe/BaseCompiler.hx file to view all of them + descriptions.
 		ReflectCompiler.AddCompiler(new TestCompiler(), {
 			fileOutputExtension: ".testout",
 			outputDirDefineName: "testoutput",
@@ -27,14 +44,25 @@ class TestCompiler extends BaseCompiler {
 			smartDCE: true
 		});
 
+		// Example of ClassModifier
+		// Modifies MyClass.testMod to return 9999.
 		ClassModifier.mod("MyClass", "testMod", macro {
 			return 9999;
 		});
 	}
 
+	// This is the function from the BaseCompiler to override to compile Haxe classes.
+	// Given the haxe.macro.ClassType and its variables and fields, return the output String.
+	// If `null` is returned, the class is ignored and nothing is compiled for it.
+	//
+	// https://api.haxe.org/haxe/macro/ClassType.html
 	public function compileClassImpl(classType: ClassType, varFields: ClassFieldVars, funcFields: ClassFieldFuncs): Null<String> {
+		// `getNameOrNative` is from `reflaxe.helpers.NameMetaHelper`.
+		// Works with any object that matches { name: String, meta: MetaAccess }
+		// Returns the value provided in `@:native` if that meta exists, and `name` otherwise.
 		var decl = "class " + classType.getNameOrNative() + ":\n";
 
+		// Iterate through the variables and compile them.
 		var varString = "";
 		for(vf in varFields) {
 			final field = vf.field;
@@ -47,6 +75,7 @@ class TestCompiler extends BaseCompiler {
 			varString += (variableDeclaration + testScriptVal).tab() + "\n";
 		}
 
+		// Iterate through the functions and compile them.
 		var funcString = "";
 		for(ff in funcFields) {
 			final field = ff.field;
@@ -55,16 +84,32 @@ class TestCompiler extends BaseCompiler {
 			funcString += (funcHeader + compileClassFuncExpr(tfunc.expr).tab()).tab() + "\n\n";
 		}
 
+		// Combine all the compiled content together and return it.
 		final body = (varString.length > 0 ? varString + "\n" : "") + funcString;
 		return decl + (body.length > 0 ? body : "\tpass");
 	}
 
+	// Works just like `compileClassImpl`, but for Haxe enums.
+	// Since we're returning `null` here, all Haxe enums are ignored.
+	//
+	// https://api.haxe.org/haxe/macro/EnumType.html
 	public function compileEnumImpl(enumType: EnumType, constructs: EnumOptions): Null<String> {
 		return null;
 	}
 
+	// This is the final required function.
+	// It compiles the expressions generated from Haxe.
+	//
+	// PLEASE NOTE: to recusively compile sub-expressions, use "compileExpression".
+	// That function handles optimizations and automated Reflaxe features
+	// before finally passing the `TypedExpr` to "compileExpressionImpl".
+	//
+	// https://api.haxe.org/haxe/macro/TypedExpr.html
 	public function compileExpressionImpl(expr: TypedExpr): Null<String> {
 		var result = "";
+
+		// Compiling the `TypedExpr` is simple. Check the `TypedExprDef` and act accordingly.
+		// https://api.haxe.org/haxe/macro/TypedExprDef.html
 		switch(expr.expr) {
 			case TConst(constant): {
 				result = constantToTestScript(constant);
@@ -207,6 +252,13 @@ class TestCompiler extends BaseCompiler {
 		return result;
 	}
 
+	// This is a special function written for the TestCompiler.
+	// Helps organize how block scopes are compiled.
+	// Highly recommend copy this and use it for yourself.
+	//
+	// Note the "tab" function is for Strings.
+	// It comes from `reflaxe.helpers.SyntaxHelper`.
+	// It simply adds tabs to every line in the String.
 	function toIndentedScope(e: TypedExpr): String {
 		return switch(e.expr) {
 			case TBlock(el): {
@@ -236,6 +288,7 @@ class TestCompiler extends BaseCompiler {
 		return "";
 	}
 
+	// Compiles TConst
 	function binopToTestScript(op: Binop, e1: TypedExpr, e2: TypedExpr): String {
 		final expr1 = compileExpression(e1);
 		final expr2 = compileExpression(e2);
@@ -243,14 +296,22 @@ class TestCompiler extends BaseCompiler {
 		return expr1 + " " + operatorStr + " " + expr2;
 	}
 
+	// Compiles TUnop
 	function unopToTestScript(op: Unop, e: TypedExpr, isPostfix: Bool): String {
 		final expr = compileExpression(e);
 		final operatorStr = OperatorHelper.unopToString(op);
 		return isPostfix ? (expr + operatorStr) : (operatorStr + expr);
 	}
 
+	// Compiles TField
 	function fieldAccessToTestScript(e: TypedExpr, fa: FieldAccess): String {
 		final expr = compileExpression(e);
+
+		// Each version of FieldAccess has different information.
+		// In this case, we're simply trying to extract the name,
+		// but you can compile different "accesses" differently.
+		//
+		// (For example, `obj.func` for instance access vs `obj::func` for static access).
 		final fieldName = switch(fa) {
 			case FInstance(_, _, classFieldRef): classFieldRef.get().name;
 			case FStatic(_, classFieldRef): classFieldRef.get().name;
@@ -262,6 +323,7 @@ class TestCompiler extends BaseCompiler {
 		return expr + "." + fieldName;
 	}
 
+	// Used to compile types in TTypeExpr and TCast
 	function moduleNameToTestScript(m: ModuleType): String {
 		return m.getNameOrNative();
 	}
