9d3f1f146352d9b2c5ff934d11ac3b914c5908f3
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-03-19 05:29:10 -0400

    Added "tabCount" param to SyntaxHelper.tab

--- src/reflaxe/helpers/SyntaxHelper.hx
+++ src/reflaxe/helpers/SyntaxHelper.hx
@@ -10,7 +10,13 @@ package reflaxe.helpers;
 #if (macro || reflaxe_runtime)
 
 class SyntaxHelper {
-	public static function tab(s: String): String {
+	public static function tab(s: String, tabCount: Int = 1): String {
+		var tabStr = "\t";
+		if(tabCount > 1) {
+			for(i in 1...tabCount) {
+				tabStr += "\t";
+			}
+		}
 
 		// maybe replace with:
 		// return StringTools.replace(s, "\n", "\n\t");
@@ -18,7 +24,7 @@ class SyntaxHelper {
 
 		final lines = s.split("\n");
 		for(i in 0...lines.length) {
-			lines[i] = "\t" + lines[i];
+			lines[i] = tabStr + lines[i];
 		}
 		return lines.join("\n");
 	}
