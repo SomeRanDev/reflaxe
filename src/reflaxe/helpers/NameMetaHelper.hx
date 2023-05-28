145b7d2fe40ca09799b1766477b938d2d994baf1
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-27 20:53:46 -0400

    Added NameMetaHelper.setNativeNameOverride + docs

--- src/reflaxe/helpers/NameMetaHelper.hx
+++ src/reflaxe/helpers/NameMetaHelper.hx
@@ -26,10 +26,22 @@ typedef NameAndMeta = {
 };
 
 class NameMetaHelper {
+	static var nativeNameOverrides: Map<String, String> = [];
+
+	/**
+		Returns `true` if a meta of `metaName` exists on the `MetaAccess`
+		meta property.
+	**/
 	public static function hasMeta(v: NameAndMeta, metaName: String) {
 		return v.meta != null && v.meta.has != null && v.meta.has(metaName);
 	}
 
+	/**
+		Given an object with the properties: `{ name: String, meta: MetaAccess }`,
+		this function will return the first argument of the meta of name
+		`metaName`. If no meta of that name can be found, the `name`
+		property is returned otherwise.
+	**/
 	public static function getNameOrMeta(v: NameAndMeta, metaName: String): String {
 		if(hasMeta(v, metaName)) {
 			final result = v.meta.extractStringFromFirstMeta(metaName);
@@ -38,14 +50,37 @@ class NameMetaHelper {
 		return v.name;
 	}
 
+	/**
+		Returns the first argument of the `@:native` meta or the `name`
+		property of the supplied object.
+	**/
 	public static function getNameOrNative(v: NameAndMeta): String {
 		return getNameOrMeta(v, ":native");
 	}
 
+	/**
+		If this object has a `@:nativeName` meta, the first argument
+		of that meta is returned.
+
+		This also retrieves any overrides configured with init macros.
+	**/
 	public static function getNameOrNativeName(v: NameAndMeta): String {
+		if(hasMeta(v, ":nativeName")) {
+			final result = v.meta.extractParamsFromFirstMeta(":nativeName");
+			if(result != null && result.length > 1) {
+				final id = result[1];
+				if(nativeNameOverrides.exists(id)) {
+					return nativeNameOverrides.get(id);
+				}
+			}
+		}
 		return getNameOrMeta(v, ":nativeName");
 	}
 
+	/**
+		Returns an instance of `NameMeta` from the content found in a 
+		`FieldAccess`.
+	**/
 	public static function getFieldAccessNameMeta(fa: FieldAccess): NameAndMeta {
 		return switch(fa) {
 			case FInstance(_, _, classFieldRef): classFieldRef.get();
@@ -55,6 +90,13 @@ class NameMetaHelper {
 			case FEnum(_, enumField): enumField;
 			case FDynamic(s): { name: s, meta: null };
 		}
+	}
+
+	/**
+		Call to override all `@:nativeName` values with a certain ID.
+	**/
+	public static function setNativeNameOverride(id: String, overrideValue: String) {
+		nativeNameOverrides.set(id, overrideValue);
 	}
 }
 
