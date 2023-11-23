bbc564ef23b3b15c6ce46df5f72842f6a7fb8104
RoBBoR <28355157+RobertBorghese@users.noreply.github.com>
2023-11-23 05:45:20 -0500

    Fixed TempVarRemover not working in sub-scopes

--- src/reflaxe/compiler/TemporaryVarRemover.hx
+++ src/reflaxe/compiler/TemporaryVarRemover.hx
@@ -3,21 +3,33 @@ package reflaxe.compiler;
 import haxe.macro.Type;
 
 using reflaxe.helpers.NameMetaHelper;
+using reflaxe.helpers.NullHelper;
 using reflaxe.helpers.TypedExprHelper;
 
 class TemporaryVarRemover {
 	/**
-		The original expression passed
+		The original expression passed.
 	**/
 	var expr: TypedExpr;
 
 	/**
-		The original expression extracted as a TBlock list
+		The original expression extracted as a TBlock list.
 	**/
 	var exprList: Array<TypedExpr>;
 
-	public function new(expr: TypedExpr) {
+	/**
+		The `TemporaryVarRemover` that created this instance.
+	**/
+	var parent: Null<TemporaryVarRemover>;
+
+	/**
+		A map of all the variables that are being removed.
+	**/
+	var tvarMap: Map<Int, TypedExpr> = [];
+
+	public function new(expr: TypedExpr, parent: Null<TemporaryVarRemover> = null) {
 		this.expr = expr;
+		this.parent = parent;
 
 		exprList = switch(expr.expr) {
 			case TBlock(exprs): exprs.map(e -> e.copy());
@@ -30,7 +42,6 @@ class TemporaryVarRemover {
 	**/
 	public function fixTemporaries(): TypedExpr {
 		final result = [];
-		final tvarMap: Map<Int, TypedExpr> = [];
 
 		for(i in 0...exprList.length) {
 			if(i < exprList.length - 1) {
@@ -38,7 +49,7 @@ class TemporaryVarRemover {
 					case TVar(tvar, maybeExpr) if(isField(maybeExpr)): {
 						switch(tvar.t) {
 							case TInst(clsRef, _) if(clsRef.get().hasMeta(":avoid_temporaries")): {
-								tvarMap.set(tvar.id, maybeExpr);
+								tvarMap.set(tvar.id, maybeExpr.trustMe());
 								continue;
 							}
 							case _:
@@ -51,10 +62,15 @@ class TemporaryVarRemover {
 			result.push(exprList[i]);
 		}
 
-		function mapTypedExpr(mappedExpr) {
+		function mapTypedExpr(mappedExpr): TypedExpr {
 			switch(mappedExpr.expr) {
-				case TLocal(v) if(tvarMap.exists(v.id)): {
-					return tvarMap.get(v.id);
+				case TLocal(v): {
+					final e = findReplacement(v.id);
+					if(e != null) return e;
+				}
+				case TBlock(exprs): {
+					final tvr = new TemporaryVarRemover(mappedExpr, this);
+					return tvr.fixTemporaries();
 				}
 				case _:
 			}
@@ -75,5 +91,19 @@ class TemporaryVarRemover {
 			case TField(e, _): true;
 			case _: false;
 		}
+	}
+
+	/**
+		If the variable ID exists in the map for this instance of any of
+		the parents, its expression will be returned. `null` otherwise.
+	**/
+	function findReplacement(variableId: Int): Null<TypedExpr> {
+		if(tvarMap.exists(variableId)) {
+			return tvarMap.get(variableId).trustMe();
+		} else if(parent != null) {
+			final e = parent.findReplacement(variableId);
+			if(e != null) return e;
+		}
+		return null;
 	}
 }
