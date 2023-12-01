49d733ef45d39eb5bc3329126ad8fcc28474e1e9
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-12-01 12:13:06 -0500

    Fix various TypedExprOrString issues

--- src/reflaxe/data/TypedExprOrString.hx
+++ src/reflaxe/data/TypedExprOrString.hx
@@ -1,5 +1,7 @@
 package reflaxe.data;
 
+import haxe.macro.Type;
+
 enum ITypedExprOrString {
 	Expression(expr: TypedExpr);
 	String(s: String);
@@ -14,8 +16,8 @@ abstract TypedExprOrString(ITypedExprOrString) {
 		this = input;
 	}
 
-	@:from public static function fromTypedExpression(expr: TypedExpr) return new TypedExprOrString(expr);
-	@:from public static function fromString(s: String) return new TypedExprOrString(s);
+	@:from public static function fromTypedExpression(expr: TypedExpr) return new TypedExprOrString(Expression(expr));
+	@:from public static function fromString(s: String) return new TypedExprOrString(String(s));
 
 	public function isExpression() return switch(this) { case Expression(_): true; case _: false; }
 	public function isString() return switch(this) { case String(_): true; case _: false; }
