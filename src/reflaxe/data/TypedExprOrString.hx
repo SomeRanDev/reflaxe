2a9bc9ffa50e8f838923f961abc7b27444474d3e
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-12-01 12:10:41 -0500

    Added TypedExprOrString (unused?)

---
+++ src/reflaxe/data/TypedExprOrString.hx
@@ -0,0 +1,26 @@
+package reflaxe.data;
+
+enum ITypedExprOrString {
+	Expression(expr: TypedExpr);
+	String(s: String);
+}
+
+/**
+	TODO: This is currently an unused type.
+	Maybe delete??
+**/
+abstract TypedExprOrString(ITypedExprOrString) {
+	inline function new(input: ITypedExprOrString) {
+		this = input;
+	}
+
+	@:from public static function fromTypedExpression(expr: TypedExpr) return new TypedExprOrString(expr);
+	@:from public static function fromString(s: String) return new TypedExprOrString(s);
+
+	public function isExpression() return switch(this) { case Expression(_): true; case _: false; }
+	public function isString() return switch(this) { case String(_): true; case _: false; }
+
+	public function getExpression() return switch(this) { case Expression(e): e; case _: throw "Not expression"; }
+	public function getString() return switch(this) { case String(s): s; case _: throw "Not string"; }
+	public function getEnum(): ITypedExprOrString return this;
+}
