e406ba62a7c0acbce28a8124790fa5eb16a58d62
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-01-17 00:03:42 -0500

    Add ExprHelper.getIdentifierDotPath

--- src/reflaxe/helpers/ExprHelper.hx
+++ src/reflaxe/helpers/ExprHelper.hx
@@ -19,6 +19,18 @@ class ExprHelper {
 		}
 	}
 
+	public static function getIdentifierDotPath(e: Expr): Null<String> {
+		return switch(e.expr) {
+			case EConst(CIdent(ident)): ident;
+			case EField(e2, field, _): {
+				final innerId = getIdentifierDotPath(e2);
+				if(innerId == null) null;
+				else '$innerId.$field';
+			}
+			case _: null;
+		}
+	}
+
 	public static function unwrapParenthesisMetaAndStoredType(e: Expr): Expr {
 		return switch(e.expr) {
 			case EMeta({ name: ":storedTypedExpr" }, _): {
