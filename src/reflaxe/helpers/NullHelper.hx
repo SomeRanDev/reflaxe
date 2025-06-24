d03d2e77ce9f6292253f1ddd464e99f80ab88f84
Maybee Rezbit <28355157+SomeRanDev@users.noreply.github.com>
2025-06-24 02:13:37 -0400

    Added `NullHelper.orError`

--- src/reflaxe/helpers/NullHelper.hx
+++ src/reflaxe/helpers/NullHelper.hx
@@ -7,17 +7,19 @@ package reflaxe.helpers;
 #if (macro || reflaxe_runtime)
 
 /**
-	`MetaAccess` can be annoying sometimes because the functions themselves may be `null`.
-	These helper functions wrap around the normal `MetaAccess` functions and ensure
-	they are not null before calling.
+	Helpers for `Null<T>` types.
 **/
 class NullHelper {
-	public static function or<T>(maybe: Null<T>, defaultVal: T): T {
+	public static inline function or<T>(maybe: Null<T>, defaultVal: T): T {
 		return maybe != null ? maybe : defaultVal;
 	}
 
-	public static function trustMe<T>(maybe: Null<T>): T {
-		if(maybe == null) throw "Trusted on null value.";
+	public static inline function trustMe<T>(maybe: Null<T>): T {
+		return orError(maybe, "Trusted on null value.");
+	}
+
+	public static inline function orError<T>(maybe: Null<T>, errorString: String): T {
+		if(maybe == null) throw errorString;
 		@:nullSafety(Off) return maybe;
 	}
 }
